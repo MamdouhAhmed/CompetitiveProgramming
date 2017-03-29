4eb675b1c49b9be664fd2ab4710661cef58ef544
Mamdouh Ahmed Moustafa <mamdouh64@gmail.com>
2017-03-29 06:19:15 +0200

    seems fair enough, really.

---
+++ SPOJ/SHOP.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+///////////////////// Solution Code
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+		#ifdef _WIN64
+		freopen("C:\\Users\\mamdouh\\Desktop\\a1.in","r",stdin);
+		freopen("C:\\Users\\mamdouh\\Desktop\\A2.out","w",stdout);
+		#elif __linux__
+		freopen("/media/mamdouh/System/Users/Mamdouh/Desktop/a1.in","r",stdin);
+		freopen("/media/mamdouh/System/Users/Mamdouh/Desktop/A2.out","w",stdout);
+		#endif
+	#endif
+	
+	return 0;
+}
