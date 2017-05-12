4bc67d3432595afd20085c9dfdf6f58b58cc0f08
Mamdouh Ahmed Moustafa <mamdouh64@gmail.com>
2017-05-12 17:10:05 +0200

    uva 1210

---
+++ UVA/1210.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+#define forr(i, n) for(int i=0;i<(n);i++)
+#define forv(i, v) for(int i=0;i<(int)v.size();i++)
+#define fords(it, ds) for(auto it = ds.begin();it!=ds.end();it++)
+#define OO (ll)1e18
+#define PI 3.14159265358979323846264
+#define fr first
+#define se second
+#define II pair<int, int>
+#define pb push_back
+#define dist(x,y,xx,yy) sqrt(((x-xx)*(x-xx))+((y-yy)*(y-yy)))
+ull gcd (ull a,ull b){ull c;while(a!=0){c=a;a=b%a;b=c;}return b;}
+///////////////////// Solution Code
+int arr[4000],p=0;
+bool np[10041];
+int countt[6222222];
+int tmp;
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+	#ifdef _WIN64
+	freopen("C:\\Users\\mamdouh\\Desktop\\a1.in","r",stdin);
+	freopen("C:\\Users\\mamdouh\\Desktop\\A2.out","w",stdout);
+	#elif __linux__
+	freopen("/media/mamdouh/System/Users/Mamdouh/Desktop/a1.in","r",stdin);
+	freopen("/media/mamdouh/System/Users/Mamdouh/Desktop/A2.out","w",stdout);
+	#endif
+	#endif
+	memset(np,1,sizeof np);
+	np[0]=np[1]=0;
+	forr(i,10041)
+	{
+		if(np[i])
+		{
+			arr[p++]=i;
+			for(int j=i*2;j<10041;np[j]=0,j+=i);
+		}
+	}
+	forr(i,p)
+	{
+		tmp=0;
+		for(int j=i;j<p;j++)
+		{
+			tmp+=arr[j];
+			countt[tmp]++;
+		}
+	}
+	while(cin>>tmp && tmp && cout<<countt[tmp]<<'\n');
+		
+	return 0;
+}
